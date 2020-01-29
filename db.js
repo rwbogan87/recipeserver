da0fbdd30ff0ab8f3d0b417fee64e4028f874b7d
ryan <user@superbook.com>
2020-01-29 14:49:19 -0500

    deploying to heroku

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('recipebible', 'postgres', 'ryanpostgres!', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
