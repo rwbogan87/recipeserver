d4abad937259a724af1686cae614bec77bd91c34
ryan <user@superbook.com>
2020-02-08 08:10:48 -0500

    added required to recipe model

--- models/recipe.js
+++ models/recipe.js
@@ -2,27 +2,27 @@ module.exports = (sequelize, DataTypes) => {
     const Recipe = sequelize.define('recipe', {
         recipeName: {
             type: DataTypes.STRING,
-            allowNull: true
+            allowNull: false
         },
         recipeCategory: {
             type: DataTypes.STRING,
-            allowNull: true
+            allowNull: false
         },
         recipeIngredients: {
             type: DataTypes.STRING,
-            allowNull: true,
+            allowNull: false,
         },
         recipeInstructions: {
             type: DataTypes.STRING,
-            allowNull: true
+            allowNull: false
         },
         recipePublic: {
             type: DataTypes.BOOLEAN,
-            allowNull: true
+            allowNull: false
         },
         chef: {
             type: DataTypes.STRING,
-            allowNull: true
+            allowNull: false
         }
     })
 return Recipe;
